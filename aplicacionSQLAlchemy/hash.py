7c3babbf968de3ee2268d6715e0da59291cf34dd
JulianMolina <73005582+Juleeeaaannn@users.noreply.github.com>
2022-06-30 09:05:05 -0300

    Delete hash.py

--- aplicacionSQLAlchemy/hash.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-if __name__ == "__main__": 
-    # cifrado de la clave utilizando md5  	    
-    clave = 'julia459' 
-    result = hashlib.md5(bytes(clave, encoding='ansi')) 
-    # muestra la clave cifrada en hexadecimal, esta es la que se guarda en base de datos 
-    print(result.hexdigest()) 
